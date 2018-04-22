dc0b9846401c56b507293facae0c76818e18c3a4
awestlund <32402500+awestlund@users.noreply.github.com>
2018-04-22 17:31:11 -0500

    some more stuff

--- Main.java
+++ Main.java
@@ -68,6 +68,57 @@ public class Main extends Application {
 			primaryStage.setScene(scene1);
 			primaryStage.setTitle("Tournament Bracket");	
 			primaryStage.show();
+						BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,600,600,Color.BLACK);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//scene.
+			//scene.
+			primaryStage.setTitle("Bracket");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			//create controls
+			BorderPane paneOuterMost = new BorderPane();
+			
+			Label header = new Label ("Welcome to Your Tournament Bracket!");
+			Rectangle r = new Rectangle(20,20,250,250);
+		       r.setFill(Color.BLUE);
+		       //root.getChildren().add(r);
+		    Label teamName = new Label("Team1");
+		    Label teamName2 = new Label("Team2");
+		    
+		    TextField enterScores = new TextField();
+		    		enterScores.setPromptText("Final Score");
+		    	TextField enterScores2 = new TextField();
+		    		enterScores2.setPromptText("Final Score");
+		    		
+		    HBox teamScore1 = new HBox();
+		    		teamScore1.getChildren().addAll(teamName, enterScores);
+		    HBox teamScore2 = new HBox();
+		    		teamScore2.getChildren().addAll(teamName2, enterScores2);
+		    		
+		    	Button submitButton = new Button("Submit");
+		    	Button submitButton2 = new Button("Submit");
+		    	
+		    VBox matchBox = new VBox();
+		    		matchBox.getChildren().addAll(teamScore1, submitButton);
+		    	VBox matchBox2 = new VBox();
+		    		matchBox2.getChildren().addAll(teamScore2, submitButton2);
+		    		
+		    	//paneOuterMost.getChildren().addAll(matchBox, header);
+		    	paneOuterMost.setTop(header);
+		    paneOuterMost.setLeft(matchBox);
+		    //center
+		    paneOuterMost.setRight(matchBox2);
+		    //bottom
+		   
+			//set properties on controls
+			//add functionality on controls
+			//add controls to panels
+		    //root.getChildren().addAll(paneOuterMost);
+//		    root.setLeft(r);
+//			root.setRight(r);
+			//root.setTop(header);
+			root.setCenter(paneOuterMost);
 			
 		} catch(Exception e) {
 			e.printStackTrace();
