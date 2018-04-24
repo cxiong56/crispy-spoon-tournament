7effb02bae5ce68662248d81d25c521fe026cdde
hwieland <36777355+hwieland@users.noreply.github.com>
2018-04-23 20:07:57 -0500

    BuildBracket completed

--- HeapBracket.java
+++ HeapBracket.java
@@ -1,9 +1,18 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HeapBracket {
 
+	protected int numTeams = 0;
+	protected int numMatches;
+	protected Team[] bracketArray;
 	/**
 	 * Generates the correct number of matches based on the number of teams and
 	 * places them in the first round based on their seed value
@@ -12,8 +21,40 @@ public class HeapBracket {
 	 * @return
 	 */
 	public boolean buildBracket(File inputFile) {
-		return false;
-
+		BufferedReader br;
+		List<String> teamList = new ArrayList<String>();
+		String input;
+		
+		try {
+			br = new BufferedReader(new FileReader(inputFile));
+		} catch (FileNotFoundException e) {
+			return false;
+		}
+		
+		try {
+			while (br.ready()) {
+				input = br.readLine();
+				if(input != null) teamList.add(input);
+			}
+			br.close();
+		} catch (IOException e) {
+			return false;
+		}
+		
+		numTeams = teamList.size();
+		bracketArray = new Team[(numTeams * 2) - 1];
+		int indexA = 0;
+		int indexB = numTeams - 1;
+		
+		for( int i = (numTeams - 1); i < bracketArray.length; i+=2) {
+			bracketArray[i] = new Team(teamList.get(indexA));
+			if( numTeams > 1) {
+				bracketArray[i + 1] = new Team(teamList.get(indexB));
+			}
+			indexA++;
+			indexB--;
+		}
+		return true;
 	}
 
 	/**
