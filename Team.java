762942a6f70dee133a97c2537e74637be7976622
awestlund <32402500+awestlund@users.noreply.github.com>
2018-04-29 15:44:07 -0500

    has seed

--- Team.java
+++ Team.java
@@ -2,11 +2,59 @@ package application;
 
 public class Team {
 	protected String name;
+	protected int score;
+	protected int seed;
+
 	/**
 	 * @param name
 	 * @param score
 	 */
-	public Team(String name) {
+	public Team(String name, int seed, int score) {
+		this.name = name;
+		this.score = score;
+		//makeTeam(name);
+	}
+
+	/**
+	 * @param name
+	 */
+	public Team(String name, int seed) {
+		this.name = name;
+		this.seed = seed;
+		this.score = 0;
+	}
+	public void setSeed(int seed){
+		this.seed = seed;
+	}
+	
+	public int getSeed(){
+		return seed;
+	}
+	/**
+	 * Sets the team's current score
+	 * 
+	 * @param score
+	 */
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	/**
+	 * Returns the teams current score
+	 * 
+	 * @return
+	 */
+	public int getScore() {
+		return score;
+
+	}
+
+	/**
+	 * Changes the name
+	 * 
+	 * @param score
+	 */
+	public void setName(String name) {
 		this.name = name;
 	}
 
@@ -17,9 +65,7 @@ public class Team {
 	 */
 	public String getName() {
 		return name;
+
 	}
-	 
-	 public String toString() {
-		 return getName();
-	 }
 }
+
