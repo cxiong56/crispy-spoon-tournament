f993fce00df2fba41ca6a9df37850fb2c547953c
Calvin1119 <brockwrob@gmail.com>
2018-05-01 19:24:19 -0500

    actually implimented Update

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 package application;
 
 import java.io.File;
-import java.io.IOException; 
+import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -28,264 +28,206 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    // debug = "on";
-    private static HeapBracket heapBracket;
-    private static Iterable<Match> allMatches;
-    Scene scene1, scene2;
+	// debug = "on";
+	private static HeapBracket heapBracket;
+	private static Iterable<Match> allMatches;
+	Scene scene1, scene2;
 
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            ScrollPane scroll = new ScrollPane();
+	@Override
+	public void start(Stage primaryStage) {
+		ScrollPane scroll = new ScrollPane();
+		scroll.setContent(Update.drawGUI(heapBracket));
+		primaryStage.setScene(new Scene(scroll));
+		primaryStage.setHeight(800);
+		primaryStage.setWidth(1250);
+		primaryStage.setTitle("Tournament Bracket");
+		primaryStage.show();
+	}
 
-            GridPane root = new GridPane();
-            int leftCol = 0;
-            int rightCol = heapBracket.numRounds() * 2 + 1;
-            int padding = 0;
-            for (int curRound = heapBracket.numRounds(); curRound > 0; curRound--, leftCol++, rightCol--, padding += heapBracket.numInRound(curRound) / 4) {
-                List<Match> round = heapBracket.getRound(curRound);
-                int left = 0;
-                int right = round.size() / 2;
-                for ( ; left < right; left++)
-                    root.add(makeMatch(round.get(left)), leftCol, left + padding);
-                for ( ; right < round.size(); right++)
-                    root.add(makeMatch(round.get(right)), rightCol, right - left + padding);
-            }
+	private static boolean answer1; // cant seem to find a simpler solution to
+									// this
 
-            scroll.setContent(root);
-            primaryStage.setScene(new Scene(scroll));
-            primaryStage.setHeight(800);
-            primaryStage.setWidth(1250);
-            primaryStage.setTitle("Tournament Bracket");
-            primaryStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	protected static boolean finalScoreEdit(Match m) {
 
-    private Node makeMatch(Match m) {
-        if (m != null)
-            return makeMatch(m.getTeam1(), m.getTeam2(),m);
-        return makeMatch("", "",m);
-    }
+		Stage stage = new Stage();
+		stage.initModality(Modality.APPLICATION_MODAL);
+		stage.setTitle("Match " + m.getNum());
+		stage.setMinWidth(300);
+		stage.setMinHeight(250);
+		GridPane trueLayout = new GridPane();
+		trueLayout.setPadding(new Insets(10, 10, 10, 10));
+		trueLayout.setVgap(8);
+		trueLayout.setHgap(10);
+		if (m.getTeam1() == null || m.getTeam2() == null) {// no match
+			Label error = new Label("A Match is not set yet.\nPlease try again later.");
+			Button close = new Button("Close");
 
-    private Node makeMatch(Team team1, Team team2, Match m) {
-        return makeMatch(team1 != null ? team1.getName() : "", team2 != null ? team2.getName() : "",m);
-    }
+			close.setOnAction(e -> stage.close());
 
-    protected Node makeMatch(String t1, String t2, Match m) {
-        VBox match = new VBox();
-        Button addScore = new Button("Add Score");
+			VBox layout = new VBox(10);
+			layout.getChildren().addAll(error, close);
+			layout.setAlignment(Pos.CENTER);
+			GridPane.setConstraints(layout, 0, 0);
+			trueLayout.getChildren().add(layout);
 
-        addScore.setOnAction( e -> {
-            //need to be able to get match number here to change the scores
-            boolean result = finalScoreEdit(m);
-            if (result == true)//final score recorded
-                addScore.setDisable(true);
-        });
-        match.getChildren().addAll(makeTeam(t1), makeTeam(t2));
-        if (t1 != "" || t2 != "") {
-            match.getChildren().add(addScore);
-        } else {
-            match.getChildren().add(new Label("                        "));//to give the empty bracket a body
-        }
-        match.setSpacing(10);
-        match.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" + "-fx-border-width: 5;"
-                        + "-fx-border-insets: 5;" + "-fx-border-radius: 5;" + "-fx-border-color: blue;");
-        return match;
-    }
+		} else {
+			// TeamA
+			Label teamA = new Label(m.getTeam1().getName());
+			TextField score1 = new TextField("0");
+			GridPane.setConstraints(teamA, 0, 0);
+			GridPane.setConstraints(score1, 1, 0);
 
-    private Node makeTeam(String string) {
-        HBox team = new HBox();
-        Label scores = new Label("   ");
-        if (string != "") {
-            scores.setText("\t\t(0)");
-        }
+			// TeamB
+			Label teamB = new Label(m.getTeam2().getName());
+			TextField score2 = new TextField("0");
+			GridPane.setConstraints(teamB, 0, 1);
+			GridPane.setConstraints(score2, 1, 1);
 
-        team.getChildren().addAll(new Label(string), scores);
-        return team;
-    }
+			// Confirm Button
+			Button confirm = new Button("Final Score");
+			confirm.setOnAction(e -> {
+				if ((score1.getText() == null || score1.getText().isEmpty())
+						|| (score2.getText() == null || score2.getText().isEmpty())) {
+					errorAlert("Error", "Please fill in the scores for both teams");
+				}
 
-    private static boolean answer1; //cant seem to find a simpler solution to this
-    private boolean finalScoreEdit(Match m) {
+				int s1, s2;
+				try {
+					s1 = Integer.parseInt(score1.getText());
+					s2 = Integer.parseInt(score2.getText());
+					if (s1 < 0 || s2 < 0) {
+						throw new NumberFormatException();
+					}
 
-        Stage stage = new Stage();
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.setTitle("Match " + m.getNum());
-        stage.setMinWidth(300);
-        stage.setMinHeight(250);
-        GridPane trueLayout = new GridPane();
-        trueLayout.setPadding(new Insets(10,10,10,10));
-        trueLayout.setVgap(8);
-        trueLayout.setHgap(10);
-        if (m.getTeam1() == null || m.getTeam2() == null) {//no match
-            Label error = new Label("A Match is not set yet.\nPlease try again later.");
-            Button close = new Button("Close");
+					boolean results = confirmScore(m, s1, s2);
+					if (results == true) {
+						m.setScore1(s1);
+						m.setScore2(s2);
+						stage.close();
+					}
+					answer1 = results;
+				} catch (NumberFormatException error) {
+					errorAlert(error.getMessage(), "Please enter positive Integers only");
+				}
 
-            close.setOnAction(e -> stage.close());
+			});
 
-            VBox layout = new VBox(10);
-            layout.getChildren().addAll(error,close);
-            layout.setAlignment(Pos.CENTER);
-            GridPane.setConstraints(layout,0,0);
-            trueLayout.getChildren().add(layout);
+			GridPane.setConstraints(confirm, 1, 2);
 
-        } else {
-            // TeamA
-            Label teamA = new Label(m.getTeam1().getName());
-            TextField score1 = new TextField("0");
-            GridPane.setConstraints(teamA, 0, 0);
-            GridPane.setConstraints(score1, 1, 0);
+			trueLayout.getChildren().addAll(teamA, score1, teamB, score2, confirm);
+		}
+		Scene scene = new Scene(trueLayout);
+		stage.setScene(scene);
+		stage.setOnCloseRequest(e -> answer1 = false);// For when user close
+														// through "x" button
+		stage.showAndWait();
 
-            // TeamB
-            Label teamB = new Label(m.getTeam2().getName());
-            TextField score2 = new TextField("0");
-            GridPane.setConstraints(teamB, 0, 1);
-            GridPane.setConstraints(score2, 1, 1);
+		return answer1;
+	}
 
-            // Confirm Button
-            Button confirm = new Button("Final Score");
-            confirm.setOnAction(e -> {
-                if ((score1.getText() == null || score1.getText().isEmpty())
-                                ||(score2.getText() == null || score2.getText().isEmpty())) {
-                    errorAlert("Error","Please fill in the scores for both teams");
-                }
-                
-                int s1,s2;
-                try {
-                    s1 = Integer.parseInt(score1.getText());
-                    s2 = Integer.parseInt(score2.getText());
-                    if (s1 < 0 || s2 < 0) {
-                        throw new NumberFormatException();
-                    }
-                    
-                    boolean results = confirmScore(m,s1,s2);
-                    if (results == true) {
-                        m.setScore1(s1);
-                        m.setScore2(s2);
-                        stage.close();
-                    } 
-                    answer1 = results;
-                } catch (NumberFormatException error) {
-                    errorAlert(error.getMessage(),"Please enter positive Integers only");
-                }
-                
-            });
-            
-            GridPane.setConstraints(confirm, 1, 2);
+	private static boolean answer2;
 
-            trueLayout.getChildren().addAll(teamA, score1, teamB, score2, confirm);
-        }
-        Scene scene = new Scene(trueLayout);
-        stage.setScene(scene);
-        stage.setOnCloseRequest(e -> answer1 = false);//For when user close through "x" button
-        stage.showAndWait();
-        
-        return answer1;
-    }
+	private static boolean confirmScore(Match m, int s1, int s2) {
+		Stage stage = new Stage();
+		stage.initModality(Modality.APPLICATION_MODAL);
+		stage.setTitle("Confirm Final Score");
+		stage.setMinWidth(300);
+		stage.setMinHeight(250);
+		BorderPane trueLayout = new BorderPane();
 
-    private static boolean answer2;
-    private boolean confirmScore(Match m,int s1, int s2) {
-        Stage stage = new Stage();
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.setTitle("Confirm Final Score");
-        stage.setMinWidth(300);
-        stage.setMinHeight(250);
-        BorderPane trueLayout = new BorderPane();
+		String matchInfo = m.getTeam1().getName() + "\t\t(" + s1 + ")\n" + m.getTeam2().getName() + "\t\t(" + s2 + ")";
+		Label label = new Label(matchInfo);
+		Label label2 = new Label("Are you sure?");
+		Label label3 = new Label("Scores entered are final and immutable.");
 
-        String matchInfo = m.getTeam1().getName() + "\t\t(" + s1 + ")\n"
-                        + m.getTeam2().getName() + "\t\t(" + s2 + ")"; 
-        Label label = new Label(matchInfo);
-        Label label2 = new Label("Are you sure?");
-        Label label3 = new Label("Scores entered are final and immutable.");
+		// yes no button
 
-        //yes no button
+		Button yes = new Button("Yes");
+		Button no = new Button("No");
 
-        Button yes = new Button("Yes");
-        Button no = new Button("No");
+		yes.setOnAction(e -> {
+			answer2 = true;
+			stage.close();
+		});
+		no.setOnAction(e -> {
+			answer2 = false;
+			stage.close();
+		});
 
-        yes.setOnAction(e -> {
-            answer2 = true;
-            stage.close();
-        });
-        no.setOnAction(e -> {
-            answer2 = false;
-            stage.close();
-        });
+		VBox layout = new VBox(10);
+		layout.getChildren().addAll(label, label2, label3);
+		layout.setAlignment(Pos.CENTER);
+		HBox yesNO = new HBox(10);
+		yesNO.setAlignment(Pos.TOP_CENTER);
+		yesNO.getChildren().addAll(yes, no);
+		trueLayout.setCenter(layout);
+		trueLayout.setBottom(yesNO);
 
+		Scene scene = new Scene(trueLayout);
+		stage.setScene(scene);
+		stage.setOnCloseRequest(e -> answer2 = false);
+		stage.showAndWait();
+		return answer2;
+	}
 
-        VBox layout = new VBox(10);
-        layout.getChildren().addAll(label,label2, label3);
-        layout.setAlignment(Pos.CENTER);
-        HBox yesNO = new HBox(10);
-        yesNO.setAlignment(Pos.TOP_CENTER);
-        yesNO.getChildren().addAll(yes,no);
-        trueLayout.setCenter(layout);
-        trueLayout.setBottom(yesNO);
+	private static void errorAlert(String title, String message) {
+		Stage window = new Stage();
 
-        Scene scene = new Scene(trueLayout);
-        stage.setScene(scene);
-        stage.setOnCloseRequest(e -> answer2 = false);
-        stage.showAndWait();
-        return answer2;
-    }
+		window.initModality(Modality.APPLICATION_MODAL);// set it so that user
+														// only interacts with
+														// this window
+		window.setTitle(title);
+		window.setMinWidth(250);
 
-    private void errorAlert(String title, String message) {
-        Stage window = new Stage();
+		Label label = new Label(message);
+		Button close = new Button("Close window");
+		close.setOnAction(e -> window.close());
 
-        window.initModality(Modality.APPLICATION_MODAL);//set it so that user only interacts with this window
-        window.setTitle(title);
-        window.setMinWidth(250);
+		VBox layout = new VBox(10);
+		layout.getChildren().addAll(label, close);
+		layout.setAlignment(Pos.CENTER);
 
-        Label label = new Label(message);
-        Button close = new Button("Close window");
-        close.setOnAction(e -> window.close());
+		Scene scene = new Scene(layout, 300, 100);
+		window.setScene(scene);
+		java.awt.Toolkit.getDefaultToolkit().beep();// alert sound
+		window.showAndWait();// display and wait until the window is closed
+	}
 
-        VBox layout = new VBox(10);
-        layout.getChildren().addAll(label, close);
-        layout.setAlignment(Pos.CENTER);
+	public static void main(String[] args) throws IOException {
+		// read input file
+		Scanner in = new Scanner(System.in);
+		System.out.println("What is the filename? ");
+		String input = in.nextLine();
+		File inputFile = new File(input);
+		heapBracket = new HeapBracket(inputFile);
+		List<Team> teamArray = heapBracket.teams;
+		String team1 = teamArray.get(0).getName();
+		int score1 = heapBracket.getTeam(team1).getScore();
+		// add all of the matches and team to the bracket
+		// allMatches = heapBracket.getAllMatches();
 
-        Scene scene = new Scene(layout,300,100);
-        window.setScene(scene);
-        java.awt.Toolkit.getDefaultToolkit().beep();//alert sound
-        window.showAndWait();//display and wait until the window is closed
-    }
+		launch(args);
 
-    public static void main(String[] args) throws IOException {
-        // read input file
-        Scanner in = new Scanner(System.in);
-        System.out.println("What is the filename? ");
-        String input = in.nextLine();
-        File inputFile = new File(input);
-        heapBracket = new HeapBracket(inputFile);
-        List<Team> teamArray = heapBracket.teams;
-        String team1 = teamArray.get(0).getName();
-        int score1 = heapBracket.getTeam(team1).getScore();
-        // add all of the matches and team to the bracket
-        //allMatches = heapBracket.getAllMatches();
-
-        launch(args);
-
-        // launch(args);
-    }
+		// launch(args);
+	}
 }
 
-//int offset = 0;
-//int teamNum = 0;//heapBracket.numTeams;
-//int max = (int) (Math.log(teamNum) / Math.log(2)) * 2;
-//int min = 0;
-//int round = 1;
-//for (int tn = teamNum; tn >= 2; min++, max--, offset += tn / 4, tn /= 2, round++) {
+// int offset = 0;
+// int teamNum = 0;//heapBracket.numTeams;
+// int max = (int) (Math.log(teamNum) / Math.log(2)) * 2;
+// int min = 0;
+// int round = 1;
+// for (int tn = teamNum; tn >= 2; min++, max--, offset += tn / 4, tn /= 2,
+// round++) {
 
-
-//	for (int i = (tn - 1); i > heapBracket.bracketArray.length; i++) {
-//		String team1 = heapBracket.bracketArray[i].getName();
-//		String team2 = heapBracket.bracketArray[i + 1].getName();
-//		root.add(makeMatch(team1, team2), min, i + offset);
-//		String team3 = heapBracket.bracketArray[i].getName();
-//		String team4 = heapBracket.bracketArray[i + 1].getName();
-//		root.add(makeMatch(team3, team4), max, i + offset);
-//	}
+// for (int i = (tn - 1); i > heapBracket.bracketArray.length; i++) {
+// String team1 = heapBracket.bracketArray[i].getName();
+// String team2 = heapBracket.bracketArray[i + 1].getName();
+// root.add(makeMatch(team1, team2), min, i + offset);
+// String team3 = heapBracket.bracketArray[i].getName();
+// String team4 = heapBracket.bracketArray[i + 1].getName();
+// root.add(makeMatch(team3, team4), max, i + offset);
+// }
 
 // primaryStage.setTitle("Tentative Tournament Bracket");
 // BorderPane root = new BorderPane();
@@ -350,4 +292,4 @@ public class Main extends Application {
 // newRoot.setCenter(root);
 // root = newRoot;
 
-//root.add(makeMatch("ChampA", "ChampB"), min, teamNum / 2 - 1);
+// root.add(makeMatch("ChampA", "ChampB"), min, teamNum / 2 - 1);
--- Update.java
+++ Update.java
@@ -1,6 +1,75 @@
 package application;
 
+import java.util.List;
+
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 public class Update {
+	
+	public static Node drawGUI(HeapBracket heapBracket) {
+		 GridPane root = new GridPane();
+         int leftCol = 0;
+         int rightCol = heapBracket.numRounds() * 2 + 1;
+         int padding = 0;
+         for (int curRound = heapBracket.numRounds(); curRound > 0; curRound--, leftCol++, rightCol--, padding += heapBracket.numInRound(curRound) / 4) {
+             List<Match> round = heapBracket.getRound(curRound);
+             int left = 0;
+             int right = round.size() / 2;
+             for ( ; left < right; left++)
+                 root.add(makeMatch(round.get(left)), leftCol, left + padding);
+             for ( ; right < round.size(); right++)
+                 root.add(makeMatch(round.get(right)), rightCol, right - left + padding);
+         }
+         return root;
+	}
+	
+	private static Node makeMatch(Match m) {
+        if (m != null)
+            return makeMatch(m.getTeam1(), m.getTeam2(),m);
+        return makeMatch("", "",m);
+    }
+
+    private static Node makeMatch(Team team1, Team team2, Match m) {
+        return makeMatch(team1 != null ? team1.getName() : "", team2 != null ? team2.getName() : "",m);
+    }
+
+    private static Node makeMatch(String t1, String t2, Match m) {
+        VBox match = new VBox();
+        Button addScore = new Button("Add Score");
+
+        addScore.setOnAction( e -> {
+            //need to be able to get match number here to change the scores
+            boolean result = Main.finalScoreEdit(m);
+            if (result == true)//final score recorded
+                addScore.setDisable(true);
+        });
+        match.getChildren().addAll(makeTeam(t1), makeTeam(t2));
+        if (t1 != "" || t2 != "") {
+            match.getChildren().add(addScore);
+        } else {
+            match.getChildren().add(new Label("                        "));//to give the empty bracket a body
+        }
+        match.setSpacing(10);
+        match.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" + "-fx-border-width: 5;"
+                        + "-fx-border-insets: 5;" + "-fx-border-radius: 5;" + "-fx-border-color: blue;");
+        return match;
+    }
+    
+	private static Node makeTeam(String string) {
+		HBox team = new HBox();
+		Label scores = new Label("   ");
+		if (string != "") {
+			scores.setText("\t\t(0)");
+		}
+
+		team.getChildren().addAll(new Label(string), scores);
+		return team;
+	}
 
 	/**
 	 * A constructor that will apply the users interactions to the HeapBracket,
