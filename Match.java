8011a34e8f31b8a8954241146afa0383fd50a69d
hwieland <36777355+hwieland@users.noreply.github.com>
2018-05-03 20:37:57 -0500

    Comments

--- Match.java
+++ Match.java
@@ -19,6 +19,11 @@
 
 package application;
 
+/**
+ * Contains the details of a match/game between two teams.
+ * @author Henry
+ *
+ */
 public class Match {
 	private Team team1;
 	private int score1;
@@ -28,11 +33,11 @@ public class Match {
 	private boolean finalized = false;
 
 	/**
-	 * Creates a new match between two teams
+	 * Creates a new match between two teams with the match num
 	 * 
 	 * @param team1
 	 * @param team2
-	 * @param num
+	 * @param num - what match this is
 	 */
 	public Match(Team team1, Team team2, int num) {
 		this.team1 = team1;
@@ -52,38 +57,71 @@ public class Match {
 		this.matchNum = 0;
 	}
 	
+	/**
+	 * @return - returns team1
+	 */
 	public Team getTeam1() {
 		return team1;
 	}
 	
+	/**
+	 * Sets team1 to team t
+	 * @param t - provided team
+	 */
 	public void setTeam1(Team t) {
 		team1 = t;
 	}
 	
+	/**
+	 * Sets score1 to score s
+	 * @param s
+	 */
 	public void setScore1(int s) {
 	    score1 = s;
 	}
 	
+	/**
+	 * @return - returns the score for team 1
+	 */
 	public int getScore1() {
 	    return score1;
 	}
 	
+	/**
+	 * @return - returns team2
+	 */
 	public Team getTeam2() {
 		return team2;
 	}
 	
+	/**
+	 * Sets team2 to team t
+	 * @param t - provided team
+	 */
 	public void setTeam2(Team t) {
 		team2 = t;
 	}
 	
+	/**
+	 * Sets score2 to score s
+	 * @param s
+	 */
 	public void setScore2(int s) {
         score2 = s;
     }
 	
+	/**
+	 * Sets score2 to score s
+	 * @param s
+	 */
 	public int getScore2() {
 	    return score2;
 	}
 	
+	/**
+	 * 
+	 * @return - the match number of this match
+	 */
 	public int getNum() {
 	    return matchNum;
 	}
@@ -113,16 +151,27 @@ public class Match {
 			return isFinal() ? team2: null;//handle ties
 	}
 	
+	/**
+	 * Returns a string representation of this match
+	 */
 	public String toString() {
 		return team1.getName() + " vs. " + team2.getName() ;
 	}
 	
+	/**
+	 * Finalizes the score
+	 */
 	public void finalize() {
 	    finalized = true;
 	}
 	
+	/**
+	 * Checks if the score has been finalized
+	 * @return
+	 */
 	public boolean isFinal() {
 	    return finalized;
 	}
+
 
 }
